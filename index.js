bbcb9210d645eaeb26b5bbfc75cdeb464dfa694a
novax-dot <riveracuadradoleticia@gmail.com>
2026-06-02 19:10:06 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const { Client, GatewayIntentBits } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
 require('dotenv').config();
